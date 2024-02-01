365c5b7fadd98f1eead7c9ab600e768b1a48d607
C4S4 <casa@sweetohm.net>
2024-02-01 12:05:14 +0100

    Implemented main features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::Parser;
+use dotenv;
+use std::process::Command;
+
+/// Run command in environment loaded from dotenv file
+#[derive(Parser)]
+struct Cli {
+    /// The dotenv file to load
+    #[arg(short, long, default_value = ".env")]
+    env: std::path::PathBuf,
+    /// The command to run
+    cmd: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // parse command line arguments
+    let args = Cli::parse();
+    // load environment from dotenv file
+    dotenv::from_filename(&args.env).expect("error loading dotenv file");
+    // check command is not empty
+    if args.cmd.is_empty() {
+        return;
+    }
+    // get command and arguments arguments
+    let command = &args.cmd[0];
+    let arguments = &args.cmd[1..];
+    // run command
+    Command::new(command).args(arguments).status().expect("failed to execute process");
 }
