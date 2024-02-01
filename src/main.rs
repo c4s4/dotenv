2666d208d1401deb8488f09b3e0c5490e9012a3b
C4S4 <casa@sweetohm.net>
2024-02-01 12:25:28 +0100

    Added clear option

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use dotenv;
+use std::env;
 use std::process::Command;
 
 /// Run command in environment loaded from dotenv file
@@ -8,6 +9,9 @@ struct Cli {
     /// The dotenv file to load
     #[arg(short, long, default_value = ".env")]
     env: std::path::PathBuf,
+    /// Clear environment before loading env file
+    #[arg(short, long, default_value_t = false)]
+    clear: bool,
     /// The command to run
     cmd: Vec<String>,
 }
@@ -15,6 +19,12 @@ struct Cli {
 fn main() {
     // parse command line arguments
     let args = Cli::parse();
+    // clear environment
+    if args.clear {
+        for(key, _) in env::vars().into_iter() {
+            env::remove_var(key)
+        }
+    }
     // load environment from dotenv file
     dotenv::from_filename(&args.env).expect("error loading dotenv file");
     // check command is not empty
@@ -25,5 +35,8 @@ fn main() {
     let command = &args.cmd[0];
     let arguments = &args.cmd[1..];
     // run command
-    Command::new(command).args(arguments).status().expect("failed to execute process");
+    Command::new(command)
+        .args(arguments)
+        .status()
+        .expect("failed to execute process");
 }
