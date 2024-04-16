a99f9d9487b1c96f7b99b54c8a1852b0f94b6cad
Michel Casabianca <mcasabianca@sfbx.io>
2024-04-16 11:59:01 +0200

    Managing errors with anyhow and running command in shell

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 use std::env;
 use std::process;
 use std::process::Command;
+
+const DEFAULT_SHELL_UNIX: &str = "sh";
+const DEFAULT_SHELL_WINDOWS: &str = "cmd";
 
 /// Run command in environment loaded from dotenv file
 #[derive(Parser)]
@@ -13,9 +17,9 @@ struct Cli {
     /// Clear environment before loading env file
     #[arg(short, long, default_value_t = false)]
     clear: bool,
-    /// Run command in a shell
-    #[arg(short, long, default_value_t = false)]
-    shell: bool,
+    /// Shell to run command in (default: sh on Unix or cmd on Windows)
+    #[clap(short, long)]
+    shell: Option<String>,
     /// The command to run
     cmd: Vec<String>,
 }
@@ -23,6 +27,20 @@ struct Cli {
 fn main() {
     // parse command line arguments
     let args = Cli::parse();
+    match run(args) {
+        Ok(_) => println!("OK"),
+        Err(e) => {
+            eprintln!("ERROR {:#}", e);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn run(args: Cli) -> Result<()> {
+    // check command is not empty
+    if args.cmd.is_empty() {
+        ()
+    }
     // clear environment
     if args.clear {
         for (key, _) in env::vars().into_iter() {
@@ -31,26 +49,18 @@ fn main() {
     }
     // load environment from dotenv file
     for path in &args.env {
-        load_dotenv_file(path);
-    }
-    // check command is not empty
-    if args.cmd.is_empty() {
-        return;
+        load_dotenv_file(path)?;
     }
     // run command
-    process::exit(run_command(args.cmd, args.shell));
+    let exit_code = run_command(args.cmd, args.shell).context("running command")?;
+    process::exit(exit_code);
 }
 
 /// Load environment from dotenv file
-fn load_dotenv_file(path: &str) {
+fn load_dotenv_file(path: &str) -> Result<()> {
     // load file content
-    let content = match std::fs::read_to_string(path) {
-        Ok(content) => content,
-        Err(err) => {
-            eprintln!("ERROR loading dotenv file: {err}");
-            process::exit(1);
-        }
-    };
+    let content =
+        std::fs::read_to_string(path).with_context(|| format!("loading dotenv file: {}", path))?;
     // parse file content
     for line in content.lines() {
         let line = line.trim();
@@ -61,39 +71,25 @@ fn load_dotenv_file(path: &str) {
         let value = line.splitn(2, '=').nth(1).unwrap().trim();
         env::set_var(key, value);
     }
+    Ok(())
 }
 
 /// Run command
-fn run_command(cmd: Vec<String>, shell: bool) -> i32 {
-    if shell {
-        // run command in shell
-        if env::consts::OS == "windows" {
-            // on windows
-            match Command::new("cmd").arg("/c").arg(&cmd.join(" ")).status() {
-                Ok(status) => return status.code().unwrap(),
-                Err(err) => {
-                    eprintln!("ERROR running command: {err}");
-                    process::exit(1);
-                }
-            }
-        } else {
-            // on unix
-            match Command::new("sh").arg("-c").arg(&cmd.join(" ")).status() {
-                Ok(status) => return status.code().unwrap(),
-                Err(err) => {
-                    eprintln!("ERROR running command: {err}");
-                    process::exit(1);
-                }
-            }
+fn run_command(cmd: Vec<String>, shell: Option<String>) -> Result<i32> {
+    // run command in shell
+    if env::consts::OS != "windows" {
+        // on unix
+        let shell = shell.unwrap_or(DEFAULT_SHELL_UNIX.to_string());
+        match Command::new(shell).arg("-c").arg(&cmd.join(" ")).status() {
+            Ok(status) => return Ok(status.code().unwrap()),
+            Err(err) => anyhow::bail!(err),
         }
     } else {
-        // run command without shell
-        match Command::new(&cmd[0]).args(&cmd[1..]).status() {
-            Ok(status) => return status.code().unwrap(),
-            Err(err) => {
-                eprintln!("ERROR running command: {err}");
-                process::exit(1);
-            }
+        // on windows
+        let shell = shell.unwrap_or(DEFAULT_SHELL_WINDOWS.to_string());
+        match Command::new(shell).arg("/c").arg(&cmd.join(" ")).status() {
+            Ok(status) => return Ok(status.code().unwrap()),
+            Err(err) => anyhow::bail!(err),
         }
     }
 }
